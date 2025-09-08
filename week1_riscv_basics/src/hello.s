c791270ec0f0b3f88b8b335e88513f207ff690aa
Joseph Bak <joseph.bak31415@gmail.com>
2025-09-07 20:57:18 -0400

    Week 1: hello.S changed to target rv64I and linux

--- week1_riscv_basics/src/hello.s
+++ week1_riscv_basics/src/hello.s
@@ -1,15 +1,55 @@
-# RV32I — Venus syscalls
+
+    .section .text
+    .globl _start
+_start:
+    # write(1, msg, len)
+    li   a7, 64            # Linux RISC-V syscall: write
+    li   a0, 1             # fd = stdout
+    la   a1, msg           # buf
+    li   a2, msg_end - msg # count
+    ecall                  # → trap into kernel, handle write(), return to user mode
+
+    # exit(0)
+    li   a7, 93            # Linux RISC-V syscall: exit
+    li   a0, 0
+    ecall                  # → trap into kernel, handle exit(), process ends
+
+    .section .rodata
+msg:
+    .asciz "Hello world!\n"
+msg_end:
+
+
+
+# RV2I — Venus syscalls
 # Prints "Hello RISC-V!" then exits
 
-    .text
-    .globl __start
-__start:
-    li a0, 4          # ecall: print string
-    la a1, msg
-    ecall
+#     .text
+#     .globl __start
+# __start:
+#     li a0, 4          # ecall: print string
+#     la a1, msg
+#     ecall
 
-    li a0, 10         # ecall: exit
-    ecall
+#     li a0, 10         # ecall: exit
+#     ecall
 
-    .data
-msg: .asciiz "Hello RISC-V!\n"
+#     .data
+# msg: .asciiz "Hello RISC-V!\n"
+
+# .globl _star
+# .section .text
+
+# _start:
+#     li a0, 1
+#     la a1, hello
+#     li a2, 13
+#     li a7, 64
+#     ecall
+
+#     li a0, 2
+#     li a7, 93
+#     ecall
+
+# .section .data
+# hello: .ascii "Hello world!\n"
